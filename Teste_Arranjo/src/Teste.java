d9214913d0e49043942d5e7d68c827fcd6bd879c
EdsonBuzetti <126629330+EdsonBuzetti@users.noreply.github.com>
2023-03-28 21:12:29 -0300

    Add files via upload

---
+++ Teste_Arranjo/src/Teste.java
@@ -0,0 +1,38 @@
+public class Teste {
+    public static void alteraArranjo(int b[]){
+
+        for(int i=0; i<b.length; i++){
+            b[i] = i*2;
+        }
+
+        System.out.println("\n**Arranjo b**");
+        for (int valor : b)
+            System.out.print(valor + " - ");
+
+
+        b = new int[3];
+
+        System.out.println("\n**Novo arranjo b**");
+        for (int valor : b)
+            System.out.print(valor + " - ");
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5};
+
+
+        System.out.println("\n**Arranjo a antes da chamada do m�todo**");
+        for (int valor : a)
+            System.out.print(valor + " - ");
+
+
+        alteraArranjo(a);
+
+        System.out.println("\n**Arranjo a ap�s a chamada do m�todo**");
+        for (int valor : a)
+            System.out.print(valor + " - ");
+
+
+    }
+
+}
